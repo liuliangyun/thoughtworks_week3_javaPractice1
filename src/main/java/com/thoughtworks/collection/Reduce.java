36143ae19717c44556b5dd34241edf8e79c10b8f
unknown <you@example.com>
2018-04-08 12:30:06 +0800

    Reduce.java is running successfully!

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -2,6 +2,7 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.IntSummaryStatistics;
 import java.util.List;
 
 public class Reduce {
@@ -13,43 +14,99 @@ public class Reduce {
     }
 
     public int getMaximum() {
-        throw new NotImplementedException();
+        IntSummaryStatistics stats = arrayList.stream().mapToInt(x -> x).summaryStatistics();
+        return stats.getMax();
     }
 
     public double getMinimum() {
-        throw new NotImplementedException();
+        IntSummaryStatistics stats = arrayList.stream().mapToInt(x -> x).summaryStatistics();
+        return stats.getMin();
     }
 
     public double getAverage() {
-        throw new NotImplementedException();
+        IntSummaryStatistics stats = arrayList.stream().mapToInt(x -> x).summaryStatistics();
+        return stats.getAverage();
     }
 
     public double getOrderedMedian() {
-        throw new NotImplementedException();
+        double median;
+        int i = arrayList.size() / 2;
+        int j = arrayList.size() % 2;
+        if (j == 0) {
+            median = (double)(arrayList.get(i-1)+arrayList.get(i))/2;
+        } else {
+            median = (double)arrayList.get(i);
+        }
+        return median;
     }
 
     public int getFirstEven() {
-        throw new NotImplementedException();
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i) % 2 == 0) {
+                return arrayList.get(i);
+            }
+        }
+        return -1;
     }
 
     public int getIndexOfFirstEven() {
-        throw new NotImplementedException();
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i) % 2 == 0) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     public boolean isEqual(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        boolean flag = true;
+        int size1 = this.arrayList.size();
+        int size2 = arrayList.size();
+        if (size1 != size2) {
+            flag = false;
+        } else {
+            for (int i = 0; i < size1; i++) {
+                if (this.arrayList.get(i) != arrayList.get(i)) {
+                    flag = false;
+                }
+            }
+        }
+        return flag;
     }
 
     //实现接口SingleLink，然后再此函数内使用
     public Double getMedianInLinkList(SingleLink singleLink) {
-        throw new NotImplementedException();
+        int size = arrayList.size();
+        for (int i: arrayList) {
+            singleLink.addTailPointer(i);
+        }
+        if(size % 2 == 0) {
+            int index1 = size/2;
+            int index2 = index1+1;
+            double mid = (double) (Integer.parseInt(singleLink.getNode(index1).toString())+ Integer.parseInt(singleLink.getNode(index1+1).toString()))/2;
+            return mid;
+        }else {
+            int index = size/2 +1;
+            double mid = (double) (Integer.parseInt(singleLink.getNode(index).toString()));
+            return mid;
+        }
     }
 
     public int getLastOdd() {
-        throw new NotImplementedException();
+        for (int i = arrayList.size()-1; i >= 0; i--) {
+            if (arrayList.get(i) % 2 == 1) {
+                return arrayList.get(i);
+            }
+        }
+        return -1;
     }
 
     public int getIndexOfLastOdd() {
-        throw new NotImplementedException();
+        for (int i = arrayList.size()-1; i >= 0; i--) {
+            if (arrayList.get(i) % 2 == 1) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
